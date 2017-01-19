2a0626ba0ed11ffa12636474e7c978c6f47c7d46
Mike Kuta <mike@kutasoftware.com>
2017-01-19 15:29:43 -0500

    Removed unused functions from utils

--- utils.h
+++ utils.h
@@ -1,13 +1,7 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
-#include <cstddef>
 #include <cstdint>
-#include <iostream>
-#include <string>
-
-extern int depth;
-extern bool is_verbose;
 
 #ifdef _MSC_VER
 #define BSWAP32(x) _byteswap_ulong(x)
@@ -16,23 +10,5 @@ extern bool is_verbose;
 #else
 #define BSWAP32(x) _bswap(x)
 #endif
-
-#ifdef _MSC_VER
-#define PACK(...) __pragma(pack(push, 1)) __VA_ARGS__ __pragma(pack(pop))
-#elif defined __GNUC__
-#define PACK(...) __VA_ARGS__ __attribute__((__packed__))
-#endif
-
-void UTF16toMBS(const wchar_t* u, size_t srclen, char* mbs, size_t dstlen);
-
-void PrintFileName(const std::string& name);
-
-template <typename T>
-void VerbosePrint(const T& t) {
-  if (!is_verbose)
-    return;
-
-  std::cout << t << std::endl;
-}
 
 #endif  // UTILS_H_
